2d15b987dd5380948bd78bca0b993664fa8ebcdd
suresh <suresh.naidu.kanuri@gmail.com>
2024-07-11 16:08:16 +0530

    my first springboot commit

---
+++ src/main/java/com/sb/helloworld/FirstSbHelloworldExampleApplication.java
@@ -0,0 +1,17 @@
+package com.sb.helloworld;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSbHelloworldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSbHelloworldExampleApplication.class, args);
+
+		System.out.println("This is the first springboot application");
+
+	}
+
+}
